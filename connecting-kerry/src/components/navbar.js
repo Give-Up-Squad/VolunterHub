41336534d5a6dd898ff9b1ab84580f7a4d2f821e
Alan <alan35691@gmail.com>
2024-05-08 14:30:06 +0100

    Navbar updated

--- connecting-kerry/src/components/navbar.js
+++ connecting-kerry/src/components/navbar.js
@@ -5,6 +5,8 @@ import styles from '../mystyle.module.css';
 // all functions have to be capital letter
 //https://www.w3schools.com/react/react_css.asp
 
+
+
 const websiteLinks = [
   "Home",
   "Volunteer",
@@ -14,20 +16,22 @@ const websiteLinks = [
 function Navbar(Links) {
 
   return (
+    // This is the header along with the Navbar buttons
     <header>
     <div className={styles.navbarContainer}>
         <div id={styles.navbarTitle}>
-            <h1>Connecting Kerry</h1>
+           <img src={require("../images/logo-no-background.png")} height={100} alt="Connecting Kerry" />
         </div>
-
+        
         <section>
           <ul className={styles.navbarButtons}>
             {websiteLinks.map((link) =>(
               <li>{link}</li>))}
-              <button>Button</button>
+              <button>Login</button>
           </ul>
         </section>
     </div>
+    
     </header>
   )
 }
