5c1c7c11515161a312d8a07a845b801ddfb30d92
Alan <alan35691@gmail.com>
2024-05-14 08:49:29 +0100

    Login Page finished + start of about us page

--- connecting-kerry/src/App.js
+++ connecting-kerry/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Login from './components/login';
+import About from './components/about';
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,7 +14,7 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Login />
+        <About />
             <Routes>
                 <Route
                     path="/login"
