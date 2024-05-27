202b29d8befca3efda60303d7a7cb3e1c89acf46
Henry-0810 <munli2002@gmail.com>
2024-05-27 17:58:32 +0100

    navbar css working

--- connecting-kerry/src/components/navbar.js
+++ connecting-kerry/src/components/navbar.js
@@ -1,43 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/navbar.module.css";
 import { useNavigate } from "react-router-dom";
 
-const websiteLinks = ["Home", "Volunteer", "Opportunities"];
+const websiteLinks = [
+  { name: "Home", path: "/" },
+  { name: "Volunteer", path: "/volunteer" },
+  { name: "Opportunities", path: "/opportunities" },
+];
 
-function Navbar(Links) {
+function Navbar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const navigate = useNavigate();
 
-  const loginClick = () => {
-    navigate("/login");
+  const handleNavigation = (path) => {
+    navigate(path);
+    setIsDrawerOpen(false); // Close the drawer when a link is clicked
   };
 
-  const homeClick = () => {
-    navigate("/");
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
   };
 
-  const volClick = () => {
-    navigate("/volunteer");
-  };
   return (
     <header>
       <div className={styles.navbarContainer}>
-        <div id={styles.navbarTitle}>
+        <div className={styles.menuButton} onClick={toggleDrawer}>
+          ☰
+        </div>
+        <ul className={styles.navbarButtons} style={{ flex: "1" }}>
+          {websiteLinks.map((link) => (
+            <li key={link.name} onClick={() => handleNavigation(link.path)}>
+              {link.name}
+            </li>
+          ))}
+        </ul>
+        <div id={styles.navbarTitle} style={{ flex: "3" }}>
           <img
             src="/images/logo-no-background.png"
             height={100}
             alt="Connecting Kerry"
-            onClick={homeClick}
+            onClick={() => handleNavigation("/")}
           />
         </div>
-
-        <section>
-          <ul className={styles.navbarButtons}>
-            {websiteLinks.map((link) => (
-              <li>{link}</li>
-            ))}
-            <button onClick={loginClick}>Login</button>
-          </ul>
-        </section>
+        <div style={{ flex: "3", margin: "10px" }}>
+          <button
+            className={styles.loginButton}
+            onClick={() => handleNavigation("/login")}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+      <div className={`${styles.drawer} ${isDrawerOpen ? styles.open : ""}`}>
+        <ul className={styles.drawerButtons}>
+          {websiteLinks.map((link) => (
+            <li key={link.name} onClick={() => handleNavigation(link.path)}>
+              {link.name}
+            </li>
+          ))}
+          <button onClick={() => handleNavigation("/login")}>Login</button>
+        </ul>
       </div>
     </header>
   );
