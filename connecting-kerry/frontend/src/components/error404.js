c6272af589a6445898d8de18b47b0e01fb9bae34
Henry-0810 <munli2002@gmail.com>
2024-06-19 17:06:31 +0100

    fixed error issue, left links

--- connecting-kerry/frontend/src/components/error404.js
+++ connecting-kerry/frontend/src/components/error404.js
@@ -1,18 +1,25 @@
-// Error404.js
-import React from 'react';
-import Styles from '../styles/error404.module.css';
+import React from "react";
+import styles from "../styles/error404.module.css";
 
 function Error404() {
   return (
     <div>
       <h1>404 Error</h1>
-      <section className={Styles['error-container']}>
-        <span className={`${Styles['digit']} ${Styles['four']}`}><span className={Styles['screen-reader-text']}>4</span></span>
-        <span className={`${Styles['digit']} ${Styles['zero']}`}><span className={Styles['screen-reader-text']}>0</span></span>
-        <span className={`${Styles['digit']} ${Styles['four']}`}><span className={Styles['screen-reader-text']}>4</span></span>
+      <section className={styles["error-container"]}>
+        <span className={`${styles["digit"]} ${styles["four"]}`}>
+          <span className={styles["screen-reader-text"]}>4</span>
+        </span>
+        <span className={`${styles["digit"]} ${styles["zero"]}`}>
+          <span className={styles["screen-reader-text"]}>0</span>
+        </span>
+        <span className={`${styles["digit"]} ${styles["four"]}`}>
+          <span className={styles["screen-reader-text"]}>4</span>
+        </span>
       </section>
-      <div className={Styles['link-container']}>
-        <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/" className={Styles['more-link']}>Home Page</a>
+      <div className={styles["link-container"]}>
+        <a href="/" className={styles["more-link"]}>
+          Home Page
+        </a>
       </div>
     </div>
   );
