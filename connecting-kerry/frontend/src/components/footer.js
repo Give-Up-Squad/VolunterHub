8e6b9ddb08f39e3c1cb07933a086fd98dbb45261
Alan <alan35691@gmail.com>
2024-07-24 15:57:25 +0100

    footer terms and conditions

--- connecting-kerry/frontend/src/components/footer.js
+++ connecting-kerry/frontend/src/components/footer.js
@@ -66,21 +66,6 @@ function Footer() {
             )}
           </ul>
         </div>
-        <div className={styles.column}>
-          <h2>Contact Us</h2>
-          <ul className={styles.links}>
-            <li>
-              <a href="#faq" className={styles.footerLink}>
-                Contact
-              </a>
-            </li>
-            <li>
-              <a href="#cookies-policy" className={styles.footerLink}>
-                Garda Vetting
-              </a>
-            </li>
-          </ul>
-        </div>
         
         <div className={styles.column}>
           <h2>Social Media</h2>
@@ -129,6 +114,9 @@ function Footer() {
             </a>
           </ul>
         </div>
+      </div>
+      <div className={styles.termsAndConText}>
+        <p>© 2024 Connecting Kerry. All Rights Reserved. <a href="/privacy">Terms and Conditions.</a></p>
       </div>
     </footer>
   );
