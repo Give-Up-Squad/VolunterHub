ff215538c9f793323f2e7f34062d59ccdca8004d
Pan Mun Li <113506894+Henry-0810@users.noreply.github.com>
2024-07-26 00:50:57 +0100

    Update updateActivityStatus.js

--- connecting-kerry/app_api/jobs/updateActivityStatus.js
+++ connecting-kerry/app_api/jobs/updateActivityStatus.js
@@ -7,20 +7,18 @@ const updateActivityStatus = async () => {
     const now = new Date();
     const formattedNow = format(now, "yyyy-MM-dd HH:mm:ss");
 
-    // Update "Upcoming" to "Ongoing"
     const updateToOngoingQuery = `
       UPDATE activities
       SET activity_status = 'Ongoing'
-      WHERE activity_status = 'Upcoming' AND activity_start_date <= $1
+      WHERE activity_start_date <= $1
     `;
     const resOngoing = await client.query(updateToOngoingQuery, [formattedNow]);
     console.log(`${resOngoing.rowCount} activities updated to Ongoing.`);
 
-    // Update "Ongoing" to "Ended"
     const updateToEndedQuery = `
       UPDATE activities
       SET activity_status = 'Ended'
-      WHERE activity_status = 'Ongoing' AND activity_end_date <= $1
+      WHERE activity_end_date <= $1
     `;
     const resEnded = await client.query(updateToEndedQuery, [formattedNow]);
     console.log(`${resEnded.rowCount} activities updated to Ended.`);
