86c91fa41084f009a4ff79e8b5946a17b7d591ac
Henry-0810 <munli2002@gmail.com>
2024-07-31 14:02:05 +0100

    change

--- connecting-kerry/frontend/src/components/orgRegistration.js
+++ connecting-kerry/frontend/src/components/orgRegistration.js
@@ -1,13 +1,15 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import styles from "../styles/registerForms.module.css";
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { OrgRegistrationSchema } from "../validations/orgRegValidation";
+import LoadingPage from "./loadingPage";
 
 const OrganisationRegistration = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const {
     register,
@@ -25,6 +27,8 @@ const OrganisationRegistration = () => {
 
   const onSubmit = async (data) => {
     console.log(data);
+    setLoading(true);
+    navigate("/loading", { state: { loadingText: "Creating account..." } });
     try {
       const userCredentials = await doCreateUserWithEmailAndPassword(
         data.email,
@@ -67,78 +71,82 @@ const OrganisationRegistration = () => {
       const responseData = await response.json();
       console.log("Backend response:", responseData);
 
-      navigate("/loading", { state: { loadingText: "Creating account..." } });
-      setTimeout(() => {
-        navigate("/review", { replace: true });
-      }, 1000);
+      navigate("/review", { replace: true });
     } catch (error) {
+      setLoading(false);
       console.error("Error registering organisation:", error.message);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.registerForm}>
-      <h2>Organisation Registration</h2>
-      <div className={styles.content}>
-        <div className={styles.inputField}>
-          <input
-            type="email"
-            {...register("email")}
-            placeholder="Email"
-            required
-          />
-          {errors.email && (
-            <p className={styles.error}>{errors.email.message}</p>
-          )}
-        </div>
-        <div className={styles.inputField}>
-          <input
-            type="password"
-            {...register("password")}
-            placeholder="Password"
-            required
-          />
-          {errors.password && (
-            <p className={styles.error}>{errors.password.message}</p>
-          )}
-        </div>
-        <div className={styles.inputField}>
-          <input
-            type="text"
-            {...register("orgName")}
-            placeholder="Organisation Name"
-            required
-          />
-          {errors.orgName && (
-            <p className={styles.error}>{errors.orgName.message}</p>
-          )}
-        </div>
-        <input type="hidden" {...register("roles")} value="Organisation" />
+    <>
+      {loading ? (
+        <LoadingPage loadingText="Creating account..." />
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.registerForm}>
+          <h2>Organisation Registration</h2>
+          <div className={styles.content}>
+            <div className={styles.inputField}>
+              <input
+                type="email"
+                {...register("email")}
+                placeholder="Email"
+                required
+              />
+              {errors.email && (
+                <p className={styles.error}>{errors.email.message}</p>
+              )}
+            </div>
+            <div className={styles.inputField}>
+              <input
+                type="password"
+                {...register("password")}
+                placeholder="Password"
+                required
+              />
+              {errors.password && (
+                <p className={styles.error}>{errors.password.message}</p>
+              )}
+            </div>
+            <div className={styles.inputField}>
+              <input
+                type="text"
+                {...register("orgName")}
+                placeholder="Organisation Name"
+                required
+              />
+              {errors.orgName && (
+                <p className={styles.error}>{errors.orgName.message}</p>
+              )}
+            </div>
+            <input type="hidden" {...register("roles")} value="Organisation" />
 
-        <div className={styles.termsandConSection}>
-          <label htmlFor="confirmTerms">
-            Please confirm that you have read{" "}
-            <a href="/privacy">Terms and Conditions</a>
-          </label>
-          <input
-            type="checkbox"
-            id="confirmTerms"
-            {...register("confirmTerms", {
-              required: "You must accept the terms and conditions",
-            })}
-          />
-        </div>
-        {errors.confirmTerms && (
-          <p className={styles.error}>{errors.confirmTerms.message}</p>
-        )}
-        <a href="/login" className={styles.loginLink}>
-          Already have an account? Login here
-        </a>
-      </div>
-      <div className={styles.action}>
-        <button type="submit">Register</button>
-      </div>
-    </form>
+            <div className={styles.termsandConSection}>
+              <label htmlFor="confirmTerms">
+                Please confirm that you have read{" "}
+                <a href="/privacy">Terms and Conditions</a>
+              </label>
+              <input
+                type="checkbox"
+                id="confirmTerms"
+                {...register("confirmTerms", {
+                  required: "You must accept the terms and conditions",
+                })}
+              />
+            </div>
+            {errors.confirmTerms && (
+              <p className={styles.error}>{errors.confirmTerms.message}</p>
+            )}
+            <a href="/login" className={styles.loginLink}>
+              Already have an account? Login here
+            </a>
+          </div>
+          <div className={styles.action}>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
